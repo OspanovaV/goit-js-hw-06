ab0547c7e9bb86e175eb520b4c8a1ec8fafd7cf3
OspanovaVita <vitaospanova@ukr.net>
2022-07-08 23:11:13 +0300

    Добавляет задачку 9(рандомный цвет)

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,24 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// Задание 9
+// Напиши скрипт, который изменяет цвета фона элемента <body> через инлайн стиль
+//  при клике на button.change - color и выводит значение цвета в span.color.
+// Для генерации случайного цвета используй функцию getRandomHexColor.
+
+
+const bodyEl = document.querySelector('body');
+const buttonEl = document.querySelector('.change-color');
+const spanEl = document.querySelector('.color');
+
+buttonEl.addEventListener('click', onChangeColor);
+
+function onChangeColor() {
+  let color = getRandomHexColor();
+  document.body.setAttribute('style', `background-color: ${color}`)
+  spanEl.textContent = color;
 }
+//генерим рандомный цвет
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`
+};
+  
